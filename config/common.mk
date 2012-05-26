deefd570b2b42c43897d2b60e8ff62f4e5f67e7f
Chirayu Desai <chirayudesai1@gmail.com>
2012-05-26 12:38:26 +0530

    derp
    Change-Id: I76ae0a94e0a519e15d3f5f84d29795825bfd0b9a

--- config/common.mk
+++ config/common.mk
@@ -5,14 +5,6 @@ ifneq ($(TARGET_BOOTANIMATION_NAME),)
         vendor/cm/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
 endif
 
-ifdef CM_NIGHTLY
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=cyanogenmodnightly
-else
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=cyanogenmod
-endif
-
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_PROPERTY_OVERRIDES += \
